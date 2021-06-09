5dfefeda09bb11d944ca8e39664e68d9c57a6f9c
alexzorzella <43708160+alexzorzella@users.noreply.github.com>
2021-06-08 17:44:18 -0700

    Compacted camera shake code

--- Assets/Delta Camera Shake/DeltaCameraShake.cs
+++ Assets/Delta Camera Shake/DeltaCameraShake.cs
@@ -20,9 +20,6 @@ public class DeltaCameraShake : MonoBehaviour
 
     [Range(0, 1F)]
     public float intensity = 0F;
-    [Range(0, 1F)]
-    public float shake_length;
-
     [Range(1F, 1.2F)]
     public float decay = 1.3F;
 
@@ -38,38 +35,34 @@ public class DeltaCameraShake : MonoBehaviour
 
     private void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0))
         {
             intensity += initial_intensity;
         }
 
-        //intensity = Mathf.Clamp(intensity - Time.deltaTime * 1 / shake_length, 0, float.MaxValue);
-        intensity = intensity / decay; //Mathf.Clamp(intensity - Time.deltaTime * 1 / shake_length, 0, float.MaxValue);
+        intensity = intensity / decay;
+
         if (intensity < 0.001)
         {
             intensity = 0;
         }
 
-        Shake();
+        ShakeMath();
     }
 
-    private void Shake()
+    private void ShakeMath()
     {
         Point point = new Point();
 
-        for (int i = 0; i < 1024; i++)
-        {
-            point.angleRange = UnityEngine.Random.Range(10f, 25f);
-            point.speed = UnityEngine.Random.Range(1f, 3f);
-        }
-
-        Vector3 sourcePosition = transform.position;
+        point.angleRange = UnityEngine.Random.Range(10f, 25f);
+        point.speed = UnityEngine.Random.Range(1f, 3f);
 
         point.angle = Mathf.SmoothStep(-point.angleRange, point.angleRange, Mathf.PingPong(loopCount / 25f * point.speed, 1f));
         Vector3 jitterOffset = new Vector3(UnityEngine.Random.Range(-jitterRandXY, jitterRandXY), UnityEngine.Random.Range(-jitterRandXY, jitterRandXY), UnityEngine.Random.Range(-jitterRandZ, jitterRandZ));
         Matrix4x4 matrix = Matrix4x4.TRS(intensity * jitterOffset * CurveScale, Quaternion.Euler(0, 0, UnityEngine.Random.Range(-matrixRand, matrixRand) * AngleMultiplier), Vector3.one);
 
-        transform.position = matrix.MultiplyPoint3x4(new Vector3(0, 0, -10));
+        Vector3 matrixMult = matrix.MultiplyPoint3x4(new Vector3(0, 0, -10));
+        transform.position = new Vector3(matrixMult.x, matrixMult.y, -10);
         transform.rotation = Quaternion.Euler(jitterOffset * intensity);
     }
 }
