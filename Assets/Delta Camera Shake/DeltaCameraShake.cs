6af3ed11c4c9d80ce2591cceafe569a8b10ee4ae
alexzorzella <43708160+alexzorzella@users.noreply.github.com>
2021-06-08 19:28:29 -0700

    Migrated all to expansive

--- Assets/Delta Camera Shake/DeltaCameraShake.cs
+++ Assets/Delta Camera Shake/DeltaCameraShake.cs
@@ -1,68 +1,159 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DeltaCameraShake : MonoBehaviour
 {
-    [Range(0F, 1F)]
-    public float jitterRandXY = 0.25F;
-    [Range(0F, 1F)]
-    public float jitterRandZ = 0.25F;
-    [Range(0F, 10F)]
-    public float matrixRand = 5;
-
-    [Range(0F, 10F)]
-    public float AngleMultiplier = 1.0F;
-    [Range(0F, 10F)]
-    public float CurveScale = 5;
-    [Range(0F, 10F)]
-    public float loopCount = 1.0F;
-
-    [Range(0, 1F)]
-    public float intensity = 0F;
-    [Range(1F, 1.2F)]
-    public float decay = 1.3F;
-
-    [Range(10F, 50F)]
-    public float initial_intensity = 20F;
-
-    private struct Point
-    {
-        public float angleRange;
-        public float angle;
-        public float speed;
-    }
+    public List<ShakeProfile> activeShakes = new List<ShakeProfile>();
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if(Input.GetMouseButtonDown(0))
         {
-            intensity += initial_intensity;
+            activeShakes.Add(Shakepedia.GetProfileClone(Shakepedia.CLASSIC));
         }
 
-        intensity = intensity / decay;
-
-        if (intensity < 0.001)
-        {
-            intensity = 0;
-        }
-
-        ShakeMath();
+        UpdateCameraPosition();
     }
 
-    private void ShakeMath()
+    private void UpdateCameraPosition()
     {
-        Point point = new Point();
+        PassThroughShakes();
+    }
 
-        point.angleRange = UnityEngine.Random.Range(10f, 25f);
-        point.speed = UnityEngine.Random.Range(1f, 3f);
+    private void PassThroughShakes()
+    {
+        List<ShakeProfile> validShakes = new List<ShakeProfile>();
 
-        point.angle = Mathf.SmoothStep(-point.angleRange, point.angleRange, Mathf.PingPong(loopCount / 25f * point.speed, 1f));
-        Vector3 jitterOffset = new Vector3(UnityEngine.Random.Range(-jitterRandXY, jitterRandXY), UnityEngine.Random.Range(-jitterRandXY, jitterRandXY), UnityEngine.Random.Range(-jitterRandZ, jitterRandZ));
-        Matrix4x4 matrix = Matrix4x4.TRS(intensity * jitterOffset * CurveScale, Quaternion.Euler(0, 0, UnityEngine.Random.Range(-matrixRand, matrixRand) * AngleMultiplier), Vector3.one);
+        Vector3 finalCameraPos = Vector3.zero;
+        Quaternion finalCameraRotation = Quaternion.identity;
 
-        Vector3 matrixMult = matrix.MultiplyPoint3x4(new Vector3(0, 0, -10));
-        transform.position = new Vector3(matrixMult.x, matrixMult.y, -10);
-        transform.rotation = Quaternion.Euler(jitterOffset * intensity);
+        foreach (var activeShake in activeShakes)
+        {
+            activeShake.UpdateIntensity();
+
+            if (!activeShake.Decayed())
+            {
+                validShakes.Add(activeShake);
+                finalCameraPos += activeShake.GetCoreography().position;
+                finalCameraRotation *= activeShake.GetCoreography().rotation;
+            } 
+        }
+
+        transform.position = new Vector3(finalCameraPos.x, finalCameraRotation.y, -10);
+        transform.rotation = finalCameraRotation;
+
+        activeShakes = validShakes;
+    }
+
+    public class ShakeProfile
+    {
+        public ShakeProfile(
+            float jitterRandXY,
+            float jitterRandZ,
+            float matrixRand,
+            float angleMultiplier,
+            float curveScale,
+            float loopCount,
+            float decay,
+            float initialIntensity)
+        {
+            this.jitterRandXY = jitterRandXY;
+            this.jitterRandZ = jitterRandZ;
+            this.matrixRand = matrixRand;
+            this.angleMultiplier = angleMultiplier;
+            this.curveScale = curveScale;
+            this.loopCount = loopCount;
+            this.decay = decay;
+            this.initialIntensity = initialIntensity;
+
+            currentIntensity = this.initialIntensity;
+        }
+
+        public float jitterRandXY;
+        public float jitterRandZ;
+        public float matrixRand;
+        public float angleMultiplier;
+        public float curveScale;
+        public float loopCount;
+
+        public float currentIntensity;
+        public float decay;
+
+        public float initialIntensity;
+
+        public void UpdateIntensity()
+        {
+            currentIntensity = currentIntensity / decay;
+
+            if (currentIntensity < 0.01)
+            {
+                currentIntensity = 0;
+            }
+        }
+
+        public bool Decayed()
+        {
+            return currentIntensity < 0.01F;
+        }
+
+        private struct Point
+        {
+            public float angleRange;
+            public float angle;
+            public float speed;
+        }
+
+        public struct CameraMovementData
+        {
+            public Vector3 position;
+            public Quaternion rotation;
+        }
+
+        public CameraMovementData GetCoreography()
+        {
+            Point point = new Point();
+
+            point.angleRange = UnityEngine.Random.Range(10f, 25f);
+            point.speed = UnityEngine.Random.Range(1f, 3f);
+
+            point.angle = Mathf.SmoothStep(-point.angleRange, point.angleRange, Mathf.PingPong(loopCount / 25f * point.speed, 1f));
+            Vector3 jitterOffset = new Vector3(UnityEngine.Random.Range(-jitterRandXY, jitterRandXY),
+                                               UnityEngine.Random.Range(-jitterRandXY, jitterRandXY),
+                                               UnityEngine.Random.Range(-jitterRandZ, jitterRandZ));
+            
+            Matrix4x4 matrix = Matrix4x4.TRS(currentIntensity * jitterOffset * curveScale,
+                                             Quaternion.Euler(0, 0, UnityEngine.Random.Range(-matrixRand, matrixRand) * angleMultiplier),
+                                             Vector3.one);
+
+            Vector3 matrixMult = matrix.MultiplyPoint3x4(new Vector3(0, 0, -10));
+
+            CameraMovementData cmd = new CameraMovementData();
+
+            cmd.position = new Vector3(matrixMult.x, matrixMult.y, -10);
+            cmd.rotation = Quaternion.Euler(jitterOffset * currentIntensity);
+
+            return cmd;
+        }
+    }
+
+    public static class Shakepedia
+    {
+        public static ShakeProfile CLASSIC = new ShakeProfile(0.1F, 0.2F, 10F, 1F, 0.6F, 1F, 1F, 8F);
+        public static ShakeProfile ONE_HUNDRED = new ShakeProfile(100F, 100F, 100F, 100F, 100F, 100F, 100F, 100F);
+
+        public static ShakeProfile GetProfileClone(ShakeProfile profile)
+        {
+            return new ShakeProfile(
+                profile.jitterRandXY,
+                profile.jitterRandZ,
+                profile.matrixRand,
+                profile.angleMultiplier,
+                profile.curveScale,
+                profile.loopCount,
+                profile.decay,
+                profile.initialIntensity);
+        }
     }
 }
