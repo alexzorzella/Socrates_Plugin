0f4268a334e09f0cd251ead250b850eab4ed00de
Alex Zorzella <43708160+alexzorzella@users.noreply.github.com>
2025-11-14 18:51:14 -0800

    Updated copyright

--- Assets/Editor/DescriptionWindow.cs
+++ Assets/Editor/DescriptionWindow.cs
@@ -5,8 +5,7 @@ public class DescriptionWindow : EditorWindow {
     void OnGUI() {
         GUILayout.TextField(
             "Socrates Plugin\n" +
-            "(c) 2021-2023 Alex Zorzella, All Rights Reserved\n" +
-            "In association with Luiz-Otàvio Zorzella\n");
+            "(c) 2021-2025 Alex Zorzella in association with Luiz-Otàvio Zorzella, All Rights Reserved");
 
         GUILayout.Box(ResourceLoader.i.Socrates, GUILayout.Width(50), GUILayout.Height(90));
     }
