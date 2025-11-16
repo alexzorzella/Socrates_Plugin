e0d42ebc74b538718120147a1cf7d223a518913c
Alex Zorzella <43708160+alexzorzella@users.noreply.github.com>
2025-11-15 18:56:18 -0800

    Snap to camera view

--- Assets/Scripts/Practicality/SnapToCameraView.cs
+++ Assets/Scripts/Practicality/SnapToCameraView.cs
@@ -1,8 +1,6 @@
 using UnityEngine;
 
 public class SnapToCameraView : MonoBehaviour {
-    enum Mode { UPPER_LEFT, UPPER_RIGHT, BOTTOM_LEFT, BOTTOM_RIGHT }
-    
     readonly Vector2 offset = new Vector2(-1.75F, 0F);
     const float zValue = 2.4F;
 
