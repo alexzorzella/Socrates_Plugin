8307d7bd5c79d3059a2cec0801ef45525a095484
Alex Zorzella <43708160+alexzorzella@users.noreply.github.com>
2025-12-14 01:37:31 -0800

    Documented ResourceLoader

--- Assets/Scripts/Static/ResourceLoader.cs
+++ Assets/Scripts/Static/ResourceLoader.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
-// TODO: This could be a regular class if it had a way of instantiating objects
 public class ResourceLoader : MonoBehaviour {
 	static ResourceLoader _i;
 
@@ -20,68 +20,172 @@ public class ResourceLoader : MonoBehaviour {
 		}
 	}
 
-	public RuntimeAnimatorController LoadAnimatorController(string animatorName) {
-		RuntimeAnimatorController result = Resources.Load<RuntimeAnimatorController>(animatorName);
+	/// <summary>
+	/// Returns the path to the object given its name and the passed subfolder, accounting
+	/// for the fact that there may be no subfolder passed.
+	/// </summary>
+	/// <param name="filename"></param>
+	/// <param name="subfolder"></param>
+	/// <returns></returns>
+	static string SafePath(string filename, string subfolder = "") {
+		string result = "";
 
-		if (result == null) {
-			Debug.Log($"Controlador de animação de '{animatorName}' não existe.");
-			return null;
+		if (string.IsNullOrWhiteSpace(subfolder)) {
+			result = filename;
+		} else {
+			result = $"{subfolder}/{filename}";
 		}
 
 		return result;
 	}
-	
-	public static GameObject LoadObject(string objectName) {
-		GameObject result = Resources.Load<GameObject>(objectName);
+
+	/// <summary>
+	/// Returns an object named objectName found in any Resources folder. If specified, the function
+	/// will look into a subfolder. Multiple subfolders are supported. A slash is not required for
+	/// the deepest folder.
+	/// </summary>
+	/// <param name="objectName">The name of the object to be loaded.</param>
+	/// <param name="subfolder">The path of the object to be loaded. i.e. 'Cats' or 'Pets/Cats'.</param>
+	/// <returns></returns>
+	public static GameObject LoadObject(string objectName, string subfolder = "") {
+		string path = SafePath(objectName, subfolder);
+		
+		GameObject result = Resources.Load<GameObject>(path);
 		return result;
 	}
 
-	public static GameObject InstantiateObject(string objectName, Vector2 position, Quaternion rotation) {
+	/// <summary>
+	/// Instantiates an object with the passed name at the origin with no rotation if one is found.
+	/// </summary>
+	/// <param name="objectName"></param>
+	/// <param name="parent"></param>
+	/// <returns></returns>
+	public static GameObject InstantiateObject(string objectName, Transform parent = null) {
+		GameObject result = InstantiateObject(objectName, Vector3.zero, Quaternion.identity, parent);
+		return result;
+	}
+	
+	/// <summary>
+	/// Instantiates an object with the passed name at the given position with the given rotation if one is found.
+	/// If a parent transform is passed, it instantiates it parented to that object.
+	/// </summary>
+	/// <param name="objectName"></param>
+	/// <param name="position"></param>
+	/// <param name="rotation"></param>
+	/// <param name="parent"></param>
+	/// <returns></returns>
+	public static GameObject InstantiateObject(string objectName, Vector3 position, Quaternion rotation, Transform parent = null) {
 		GameObject loadedObject = LoadObject(objectName);
 
 		if (loadedObject == null) {
 			return null;
 		}
 
-		return Instantiate(loadedObject, position, rotation);
+		return Instantiate(loadedObject, position, rotation, parent);
 	}
 
-	public static Sprite LoadAsepriteSprite(string name) {
-		var asepriteFile = Resources.Load<GameObject>(name);
+	/// <summary>
+	/// Returns an object of type T named filename found in any Resources folder. If specified,
+	/// the function will look into a subfolder. Multiple subfolders are supported. A slash is
+	/// not required for the deepest folder.
+	/// </summary>
+	/// <param name="filename"></param>
+	/// <param name="subfolder"></param>
+	/// <returns></returns>
+	public T LoadFile<T>(string filename, string subfolder = "") where T : Object {
+		string path = SafePath(filename, subfolder);
+		
+		T result = Resources.Load<T>(path);
+
+		if (result == null) {
+			Debug.Log($"No file found at {path}.");
+			return null;
+		}
+
+		return result;
+	}
+	
+	/// <summary>
+	/// Returns an Aseprite file named filename found in any Resources folder. If specified, the function
+	/// will look into a subfolder. Multiple subfolders are supported. A slash is not required for
+	/// the deepest folder.
+	/// </summary>
+	/// <param name="filename"></param>
+	/// <param name="subfolder"></param>
+	/// <returns></returns>
+	public static Sprite LoadAsepriteSprite(string filename, string subfolder = "") {
+		string path = SafePath(filename, subfolder);
+
+		var asepriteFile = Resources.Load<GameObject>(path);
 
 		Sprite result = null;
 
 		if (asepriteFile != null) {
 			result = asepriteFile.GetComponent<SpriteRenderer>().sprite;
+		} else {
+			Debug.LogWarning($"No file found at {path}.");
 		}
 
 		return result;
 	}
 
-	public static Sprite LoadSprite(string name) {
-		Sprite result = Resources.Load<Sprite>(name);
+	/// <summary>
+	/// Returns a Sprite named filename found in any Resources folder. If specified, the function
+	/// will look into a subfolder. Multiple subfolders are supported. A slash is not required for
+	/// the deepest folder.
+	/// </summary>
+	/// <param name="filename"></param>
+	/// <param name="subfolder"></param>
+	/// <returns></returns>
+	public static Sprite LoadSprite(string filename, string subfolder = "") {
+		string path = SafePath(filename, subfolder);
+		
+		Sprite result = Resources.Load<Sprite>(path);
 
 		if (result == null) {
-			Debug.LogWarning($"Não existe um sprite no seus Recursos chamado '{name}'.");
+			Debug.LogWarning($"No sprite found at {path}.");
 			return null;
 		}
 
 		return result;
 	}
 
-	public static Sprite LoadSprite(string mainTexture, string subTextureName) {
-		Sprite[] sprites = Resources.LoadAll<Sprite>(mainTexture);
+	/// <summary>
+	/// Returns a subtexture Sprite named subtextureName whose parent Sprite is named mainTexture
+	/// found in any Resources folder. If specified, the function will look into a subfolder.
+	/// Multiple subfolders are supported. A slash is not required for the deepest folder.
+	/// </summary>
+	/// <param name="mainTexture"></param>
+	/// <param name="subtextureName"></param>
+	/// <param name="subfolder"></param>
+	/// <returns></returns>
+	public static Sprite LoadSprite(string mainTexture, string subtextureName, string subfolder = "") {
+		string path = SafePath(mainTexture, subfolder);
+		
+		Sprite[] sprites = Resources.LoadAll<Sprite>(path);
 
 		if (sprites != null) {
-			Sprite sprite = Array.Find(sprites, s => s.name == subTextureName);
+			Sprite sprite = Array.Find(sprites, s => s.name == subtextureName);
 			return sprite;
 		}
+		
+		Debug.LogError($"No subtexture named {subtextureName} found at {path}.");
 
 		return null;
 	}
 
-	public static Sprite LoadFirstSprite(string mainTexture) {
-		Sprite[] sprites = Resources.LoadAll<Sprite>(mainTexture);
+	/// <summary>
+	/// Returns the first subtexture Sprite whose parent Sprite is named mainTexture found
+	/// in any Resources folder. If specified, the function will look into a subfolder.
+	/// Multiple subfolders are supported. A slash is not required for the deepest folder.
+	/// </summary>
+	/// <param name="mainTexture"></param>
+	/// <param name="subfolder"></param>
+	/// <returns></returns>
+	public static Sprite LoadFirstSprite(string mainTexture, string subfolder = "") {
+		string path = SafePath(mainTexture, subfolder);
+		
+		Sprite[] sprites = Resources.LoadAll<Sprite>(path);
 
 		if (sprites != null) {
 			if (sprites.Length > 0) {
@@ -90,11 +194,23 @@ public class ResourceLoader : MonoBehaviour {
 			}
 		}
 
+		Debug.LogError($"No subtexture was found at {path}.");
+		
 		return null;
 	}
 
-	public static Sprite LoadRandomSprite(string mainTexture) {
-		Sprite[] sprites = Resources.LoadAll<Sprite>(mainTexture);
+	/// <summary>
+	/// Returns a random subtexture Sprite whose parent Sprite is named mainTexture found
+	/// in any Resources folder. If specified, the function will look into a subfolder.
+	/// Multiple subfolders are supported. A slash is not required for the deepest folder.
+	/// </summary>
+	/// <param name="mainTexture"></param>
+	/// <param name="subfolder"></param>
+	/// <returns></returns>
+	public static Sprite LoadRandomSprite(string mainTexture, string subfolder = "") {
+		string path = SafePath(mainTexture, subfolder);
+		
+		Sprite[] sprites = Resources.LoadAll<Sprite>(path);
 
 		if (sprites != null) {
 			if (sprites.Length > 0) {
@@ -102,6 +218,8 @@ public class ResourceLoader : MonoBehaviour {
 				return sprite;
 			}
 		}
+		
+		Debug.LogError($"No subtexture was found at {path}.");
 
 		return null;
 	}
