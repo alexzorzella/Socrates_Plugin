d1daadb07503221625f922319c9cdbec6e27a541
Alex Zorzella <43708160+alexzorzella@users.noreply.github.com>
2025-12-15 20:40:50 -0800

    Multiple conversations can be kept in the same file

--- Assets/Scripts/Socrates Dialogue/Scripts/DialogueParser.cs
+++ Assets/Scripts/Socrates Dialogue/Scripts/DialogueParser.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
-using System.Threading;
-using SocratesDialogue;
 using UnityEngine;
 
 namespace SocratesDialogue {
@@ -17,7 +15,7 @@ namespace SocratesDialogue {
         /// <param name="filename"></param>
         /// <returns></returns>
         public static DialogueSection ParseFile(string filename) {
-            List<DialogueSection> results = new();
+            List<List<DialogueSection>> results = new();
 
             var filepath = Path.Combine(Application.streamingAssetsPath, "Localization", $"{filename}.tsv");
 
@@ -31,6 +29,8 @@ namespace SocratesDialogue {
             string[] lines = contents.Split('\n');
 
             int emptyLineCount = 0;
+            int currentConversationIndex = 0;
+            results.Add(new List<DialogueSection>());
 
             for (int i = 0; i < lines.Length; i++) {
                 string line = lines[i];
@@ -46,6 +46,11 @@ namespace SocratesDialogue {
                         break;
                     }
 
+                    if (results[currentConversationIndex].Count > 0) {
+                        currentConversationIndex++;
+                        results.Add(new List<DialogueSection>());
+                    }
+
                     continue;
                 }
 
@@ -57,20 +62,26 @@ namespace SocratesDialogue {
                     uniqueReference = current.GetFacet<DialogueReference>().ToString();
                 }
 
-                if (results.Count > 0) {
+                if (results[currentConversationIndex].Count > 0) {
                     if(!current.HasFacet<DialogueReference>()) {
                         uniqueReference = DialogueManifest.GetUniqueReference();
                     }
     
                     uniqueReference = DialogueManifest.AddEntry(uniqueReference, current);
     
-                    results.Last().AddFacet(new NextSection(uniqueReference));
+                    results[currentConversationIndex].Last().AddFacet(new NextSection(uniqueReference));
                 }
                 
-                results.Add(current);
+                results[currentConversationIndex].Add(current);
             }
 
-            return results[0];
+            if (results.Count < 0 || results[0].Count < 0) {
+                return null;
+            }
+            
+            Debug.Log($"Parsed {results.Count} conversation(s) from {filename}");
+            
+            return results[0][0];
         }
 
         static readonly Regex facetReader = new(@"^([a-zA-Z]+):[ ]*(.*)$");
