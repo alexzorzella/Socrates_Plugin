20a1650dfb764c0697423636f30aa408b59b5d1b
Alex Zorzella <43708160+alexzorzella@users.noreply.github.com>
2025-12-18 16:39:43 -0800

    Documented DialogueInteraction

--- Assets/Scripts/Socrates Dialogue/Scripts/DialogueInteraction.cs
+++ Assets/Scripts/Socrates Dialogue/Scripts/DialogueInteraction.cs
@@ -1,18 +1,16 @@
-
 using SocratesDialogue;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+/// <summary>
+/// Continues the current conversation whenever the user left clicks.
+/// To change the way the user continues the conversation, just call
+/// DialogueManager.ContinueConversation() from somewhere else.
+/// </summary>
 public class DialogueInteraction : MonoBehaviour {
-    DialogueManager dialogueManager;
-
-    void Start() {
-        dialogueManager = GetComponent<DialogueManager>();
-    }
-
     void Update() {
         if (Mouse.current.leftButton.wasPressedThisFrame) {
-            dialogueManager.ContinueConversation();
+            DialogueManager.i.ContinueConversation();
         }
     }
 }
