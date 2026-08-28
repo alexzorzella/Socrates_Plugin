442fd55a2a330470709d0909720d151dca24dfba
Alex Zorzella <43708160+alexzorzella@users.noreply.github.com>
2026-08-28 02:40:08 -0700

    HcDebug has DebugView's old contents and DebugView was not up to date. Fixed both, no errors

--- Assets/Scripts/Console/HcDebug.cs
+++ Assets/Scripts/Console/HcDebug.cs
@@ -1,102 +1,42 @@
 using System.Collections.Generic;
-using TMPro;
-using UnityEngine;
-
-public class DebugView : MonoBehaviour {
-    static DebugView _i;
-
-    static readonly Dictionary<string, TextMeshProUGUI> objects = new();
-    static readonly List<string> objectNames = new();
-
-    public static DebugView i {
-        get {
-            if (_i == null) {
-                GameObject x = ResourceLoader.LoadObject("DebugView");
-
-                _i = Instantiate(x).GetComponent<DebugView>();
-                _i.Initialize();
-            }
-
-            return _i;
-        }
-    }
-
-    void Initialize() {
-        DontDestroyOnLoad(gameObject);
-
-        for (int i = 0; i < transform.childCount; i++) {
-            Transform child = transform.GetChild(i);
-
-            if (child.GetComponent<TextMeshProUGUI>() == null) {
-                continue;
-            }
-
-            string name = child.name.ToLower();
-            objects.Add(name, child.GetComponent<TextMeshProUGUI>());
-            objectNames.Add(name);
-        }
-    }
-
-    public List<string> GetObjectNames() {
-        return objectNames;
-    }
-
-    public void SetDebugTextObjectActive(string objectName, bool active) {
-        if (!objects.ContainsKey(objectName)) {
-            Debug.Log($"There isn't an object named {objectName} in DebugView");
-        }
-
-        objects[objectName].gameObject.SetActive(active);
-    }
-
-    public bool ToggleDebugTextObject(string objectName) {
-        if (!objects.ContainsKey(objectName)) {
-            Debug.Log($"There isn't an object named {objectName} in DebugView");
-            return false;
-        }
-
-        bool toggleTo = !objects[objectName].gameObject.activeSelf;
-        objects[objectName].gameObject.SetActive(toggleTo);
-        return toggleTo;
-    }
-
-    public void SetDebugText(string objectName, string debugInfo) {
-        if (!objects.ContainsKey(objectName)) {
-            Debug.Log($"There isn't an object named {objectName} in DebugView");
-            return;
-        }
-
-        objects[objectName].SetText(debugInfo);
-    }
     
-    public void ShowAll() {
-        foreach (var objectName in objectNames) {
-            SetDebugTextObjectActive(objectName, true);
-        }
+public class HcDebug : HCommand {
+    readonly List<string> options = new();
+
+    public HcDebug() {
+        options = DebugView.i.GetObjectNames();
     }
 
-    public void HideAll() {
-        foreach (var objectName in objectNames) {
-            SetDebugTextObjectActive(objectName, false);
+    public string CommandFunction(params string[] parameters) {
+        if (parameters.Length < 2) {
+            return $"Please specify of the ({options.Count}) object name(s)";
         }
-    }
 
-    public bool ShowAllIfAtLeastOneObjectInactiveOtherwiseHideAll() {
-        bool doHideAll = true;
+        string result = "";
 
-        foreach (var objectName in objectNames) {
-            if (!objects[objectName].gameObject.activeSelf) {
-                doHideAll = false;
-                break;
+        for (int i = 1; i < parameters.Length; i++) {
+            string objectName = parameters[i];
+            bool active = DebugView.i.ToggleDebugTextObject(objectName);
+
+            result += $"{objectName} is now {(active ? "shown" : "hidden")}";
+
+            if (i < parameters.Length - 1) {
+                result += "\n";
             }
         }
 
-        if (doHideAll) {
-            HideAll();
-        } else {
-            ShowAll();
-        }
+        return result;
+    }
 
-        return doHideAll;
+    public string CommandHelp() {
+        return "";
+    }
+
+    public string Keyword() {
+        return "debug";
+    }
+
+    public List<string> AutocompleteOptions() {
+        return options;
     }
 }
--- Assets/Scripts/DebugView/DebugView.cs
+++ Assets/Scripts/DebugView/DebugView.cs
@@ -1,64 +1,102 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using UnityEngine.InputSystem.Switch;
 
 public class DebugView : MonoBehaviour {
-   static DebugView _i;
-   
-   static readonly Dictionary<string, TextMeshProUGUI> children = new();
-   static readonly List<string> childNames = new();
-   
-   public static DebugView i {
-      get {
-         if (_i == null) {
-            GameObject x = ResourceLoader.LoadObject("DebugView");
+    static DebugView _i;
 
-            _i = Instantiate(x).GetComponent<DebugView>();
-            _i.Initialize();
-         }
-         
-         return _i;
-      }
-   }
+    static readonly Dictionary<string, TextMeshProUGUI> objects = new();
+    static readonly List<string> objectNames = new();
 
-   void Initialize() {
-      DontDestroyOnLoad(gameObject);
-      
-      for (int i = 0; i < transform.childCount; i++) {
-         Transform child = transform.GetChild(i);
+    public static DebugView i {
+        get {
+            if (_i == null) {
+                GameObject x = ResourceLoader.LoadObject("DebugView");
 
-         if (child.GetComponent<TextMeshProUGUI>() == null) {
-            continue;
-         }
-         
-         string name = child.name.ToLower();
-         children.Add(name, child.GetComponent<TextMeshProUGUI>());
-         childNames.Add(name); 
-      }
-   }
-	
-   public List<string> GetObjectNames() {
-      return childNames;
-   }
+                _i = Instantiate(x).GetComponent<DebugView>();
+                _i.Initialize();
+            }
 
-   public bool ToggleDebugTextObject(string objectName) {
-      if (!children.ContainsKey(objectName)) {
-         Debug.Log($"There isn't an object named {objectName} in DebugView");
-         return false;
-      }
-      
-      bool toggleTo = !children[objectName].gameObject.activeSelf;
-      children[objectName].gameObject.SetActive(toggleTo);
-      return toggleTo;
-   }
+            return _i;
+        }
+    }
 
-   public void SetDebugText(string objectName, string debugInfo) {
-      if (!children.ContainsKey(objectName)) {
-         Debug.Log($"There isn't an object named {objectName} in DebugView");
-         return;
-      }
+    void Initialize() {
+        DontDestroyOnLoad(gameObject);
 
-      children[objectName].SetText(debugInfo);
-   }
+        for (int i = 0; i < transform.childCount; i++) {
+            Transform child = transform.GetChild(i);
+
+            if (child.GetComponent<TextMeshProUGUI>() == null) {
+                continue;
+            }
+
+            string name = child.name.ToLower();
+            objects.Add(name, child.GetComponent<TextMeshProUGUI>());
+            objectNames.Add(name);
+        }
+    }
+
+    public List<string> GetObjectNames() {
+        return objectNames;
+    }
+
+    public void SetDebugTextObjectActive(string objectName, bool active) {
+        if (!objects.ContainsKey(objectName)) {
+            Debug.Log($"There isn't an object named {objectName} in DebugView");
+        }
+
+        objects[objectName].gameObject.SetActive(active);
+    }
+
+    public bool ToggleDebugTextObject(string objectName) {
+        if (!objects.ContainsKey(objectName)) {
+            Debug.Log($"There isn't an object named {objectName} in DebugView");
+            return false;
+        }
+
+        bool toggleTo = !objects[objectName].gameObject.activeSelf;
+        objects[objectName].gameObject.SetActive(toggleTo);
+        return toggleTo;
+    }
+
+    public void SetDebugText(string objectName, string debugInfo) {
+        if (!objects.ContainsKey(objectName)) {
+            Debug.Log($"There isn't an object named {objectName} in DebugView");
+            return;
+        }
+
+        objects[objectName].SetText(debugInfo);
+    }
+    
+    public void ShowAll() {
+        foreach (var objectName in objectNames) {
+            SetDebugTextObjectActive(objectName, true);
+        }
+    }
+
+    public void HideAll() {
+        foreach (var objectName in objectNames) {
+            SetDebugTextObjectActive(objectName, false);
+        }
+    }
+
+    public bool ShowAllIfAtLeastOneObjectInactiveOtherwiseHideAll() {
+        bool doHideAll = true;
+
+        foreach (var objectName in objectNames) {
+            if (!objects[objectName].gameObject.activeSelf) {
+                doHideAll = false;
+                break;
+            }
+        }
+
+        if (doHideAll) {
+            HideAll();
+        } else {
+            ShowAll();
+        }
+
+        return doHideAll;
+    }
 }
