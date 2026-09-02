0cfdfbcb9d117d53789525f9109825381ca34c87
Alex Zorzella <43708160+alexzorzella@users.noreply.github.com>
2026-09-02 15:15:35 -0700

    Cleaned up some unncessary usings in DialogueParser

--- Assets/Scripts/Socrates Dialogue/Scripts/DialogueParser.cs
+++ Assets/Scripts/Socrates Dialogue/Scripts/DialogueParser.cs
@@ -1,11 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text.RegularExpressions;
 using UnityEngine;
 using System.Runtime.CompilerServices;
-using Codice.CM.Common.Merge;
 
 [assembly: InternalsVisibleTo("EditMode")]
 
