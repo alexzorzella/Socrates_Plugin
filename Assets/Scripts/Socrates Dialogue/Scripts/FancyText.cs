29a6dea974b767d9b49df2891b73b9b7162ca23d
Alex Zorzella <43708160+alexzorzella@users.noreply.github.com>
2026-09-02 15:56:44 -0700

    Removed unnecessary usings from FancyText

--- Assets/Scripts/Socrates Dialogue/Scripts/FancyText.cs
+++ Assets/Scripts/Socrates Dialogue/Scripts/FancyText.cs
@@ -1,10 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
-using JetBrains.Annotations;
-using UnityEditor;
 using UnityEngine;
-using UnityEngine.Assertions;
 
 namespace SocratesDialogue {
     public class FancyText {
