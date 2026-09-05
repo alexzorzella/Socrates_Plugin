d56fc7cf57a40c83a13f9963b76a165775cca261
Alex Zorzella <43708160+alexzorzella@users.noreply.github.com>
2026-09-04 17:57:47 -0700

    Formatted documentation in VersionText and made it require a TextMeshProUGUI component to be attached to an object

--- Assets/Scripts/Practicality/VersionText.cs
+++ Assets/Scripts/Practicality/VersionText.cs
@@ -2,14 +2,12 @@ using System.Text.RegularExpressions;
 using TMPro;
 using UnityEngine;
 
+[RequireComponent(typeof(TextMeshProUGUI))]
 public class VersionText : MonoBehaviour {
     static readonly Regex VERSION_EXTRACTOR_REGEXP = new(@"((\d{2}.\d{2}).(\d+))(?:-(\w{4}))?");
 
-    /**
-     * Returns a version string suitable for printing as a watermark.
-     *
-     * It adds a "v" prefix, and strips the `-<sha>`.
-     */
+    // Returns a version string suitable for printing as a watermark.
+    // It adds a "v" prefix, and strips the `-<sha>`.
     static internal string PrintableVersionNoSha(string applicationVersion) {
         return "v" + VERSION_EXTRACTOR_REGEXP.Match(applicationVersion).Groups[1];
     }
